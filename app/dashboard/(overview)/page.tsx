db3ef3c2abfe2ffa3c26804d9b79c0adcd19b6b6
Mark Brown <markbrown83@googlemail.com>
2025-09-24 16:53:08 +0100

    reverse partial rendering

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,8 +9,6 @@ import { Suspense } from 'react';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-   
-
     return <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
         <div className={`grid gap-6 sm:grid-cols-2 lg:grid-cols-4`}>
